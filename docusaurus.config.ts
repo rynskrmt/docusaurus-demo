efc2c0a1c8068869a2107e387a4844da3fc96b8b
rynskrmt <rynskrmt@gmail.com>
2025-12-25 14:49:44 +0900

    chore: github pagesにデプロイするための設定を反映

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://cs-board-help.example.com',
+  url: 'https://rynskrmt.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-demo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rynskrmt', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-demo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
